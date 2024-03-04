a1c3633be48776e84b82fb5d528240e70bc117db
FelipeLEopoldino <felipeleopoldino01@icloud.com>
2024-03-04 18:49:11 -0300

    [UPDATE] Projeto Alura Studies

---
+++ src/components/Botao/index.tsx
@@ -0,0 +1,20 @@
+import React, { ReactNode } from 'react'
+import style from './Botao.module.scss'
+
+interface BotaoProps {
+   children: ReactNode
+   type?: 'button' | 'submit' | 'reset' | undefined
+}
+
+class Botao extends React.Component<BotaoProps> {
+   render() {
+      const { type = 'button' } = this.props
+      return (
+         <button type={type} className={style.botao}>
+            {this.props.children}
+         </button>
+      )
+   }
+}
+
+export default Botao
